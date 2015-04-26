30a2441a610fb0c36ce48ed501a86081ead6503d
Will Madden <w.a.madden@gmail.com>
2015-04-26 09:40:05 +0200

    Add BrowserSync and configure build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-var
-  gulp = require('gulp'),
-  sourcemaps = require("gulp-sourcemaps"),
-  babel = require("gulp-babel"),
-  concat = require("gulp-concat");
+var gulp = require('gulp');
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+var spawn = require('child_process').spawn;
+var browserSync = require("browser-sync").create();
 
-gulp.task('default', function() {
+gulp.task('default', ['build', 'watch', 'serve']);
+gulp.task('build', ['build-js', 'build-css', 'copy-static']);
+gulp.task('serve', ['browser-sync']);
+
+gulp.task('build-js', function() {
   return gulp.src("src/app.js")
     .pipe(sourcemaps.init())
     .pipe(concat("all.js"))
@@ -13,11 +18,36 @@ gulp.task('default', function() {
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task('webserver', function() {
-  gulp.src('app')
-    .pipe(server({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+gulp.task('build-css', function() {
+  return gulp.src("src/**/*.css", { base: 'src' })
+    .pipe(gulp.dest("dist"));
+});
+
+
+gulp.task('copy-static', function() {
+  return gulp.src("src/static/**/*", { base: 'src/static'  })
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.js', ['build-js']);
+  gulp.watch('src/**/*.css', ['build-css']);
+  gulp.watch('src/static/**/*', ['copy-static']);
+
+});
+
+gulp.task('browser-sync', function() {
+  // Full browser reload for JS or HTML changes
+  gulp.watch(['dist/**/*.js', 'dist/**/*.html'], browserSync.reload);
+  // Selective reload for CSS
+  gulp.watch(['dist/**/*.css'], function() {
+    return gulp.src('dist/**/*.css')
+      .pipe(browserSync.reload({ stream: true }));
+  });
+
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
 });
