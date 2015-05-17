975337cac9f9ec43db9828ff824efb8f01dfb34b
Will Madden <w.a.madden@gmail.com>
2015-05-17 18:59:35 +0200

    Add `TaskDetailScreen`

---
+++ src/js/ui/TaskDetailScreen.js
@@ -0,0 +1,52 @@
+import React from "react";
+import TasksStore from "stores/TasksStore";
+import TaskActions from "actions/TaskActions";
+import { FlatButton } from "material-ui";
+
+let TaskDetailScreen = React.createClass({
+
+  getInitialState() {
+    return this.getStateFromStores();
+  },
+
+  getStateFromStores() {
+    return {
+      task: TasksStore.getCurrentTask()
+    };
+  },
+
+  render() {
+    return <div>
+      <FlatButton onClick={ this.deselectTask }>Back</FlatButton>
+
+      <div>{ this.state.task.description }</div>
+      <div>{ this.state.task.completedPomodoros } pomodoros completed</div>
+
+      <div className="task-detail__actions">
+        <FlatButton>Start</FlatButton>
+        <FlatButton>Stop</FlatButton>
+        <FlatButton>Delete</FlatButton>
+        <FlatButton>Finished</FlatButton>
+      </div>
+    </div>;
+  },
+
+  deselectTask() {
+    TaskActions.deselectTask();
+  },
+
+  componentWillMount() {
+    TasksStore.addChangeListener(this.onChange);
+  },
+
+  componentWillUnmount() {
+    TasksStore.removeChangeListener(this.onChange);
+  },
+
+  onChange() {
+    this.setState(this.getStateFromStores());
+  }
+
+});
+
+export default TaskDetailScreen;
