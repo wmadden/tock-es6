187157e1cd659cc8ac2050b62ea4eefb516ea8e3
Will Madden <w.a.madden@gmail.com>
2015-05-17 19:01:45 +0200

    Make the `App` switch between top-level views

--- src/js/ui/App.js
+++ src/js/ui/App.js
@@ -1,13 +1,44 @@
 import React from "react";
 import TaskListScreen from "ui/TaskListScreen";
+import TaskDetailScreen from "ui/TaskDetailScreen";
+import TasksStore from "stores/TasksStore";
+
+function branch(condition, whenTrue, whenFalse) {
+  return condition ? whenTrue() : whenFalse();
+}
 
 let App = React.createClass({
+
+  getInitialState() {
+    return this.getStateFromStores();
+  },
+
+  getStateFromStores() {
+    return {
+      currentTask: TasksStore.getCurrentTask()
+    };
+  },
 
   render() {
     return <div className="app">
       <h1>Tock</h1>
-      <TaskListScreen />
+      { branch( this.state.currentTask,
+          () => <TaskDetailScreen />,
+          () => <TaskListScreen />
+        ) }
     </div>;
+  },
+
+  componentWillMount() {
+    TasksStore.addChangeListener(this.onChange);
+  },
+
+  componentWillUnmount() {
+    TasksStore.removeChangeListener(this.onChange);
+  },
+
+  onChange() {
+    this.setState(this.getStateFromStores());
   }
 
 });
