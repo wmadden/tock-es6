46f85dc404453ddd8858bee0cc32c528a906eae8
Will Madden <w.a.madden@gmail.com>
2015-05-25 14:42:10 +0200

    Extract time formatting functions into a separate service

--- src/ui/TaskDetailScreen.js
+++ src/ui/TaskDetailScreen.js
@@ -3,35 +3,9 @@ import TasksStore from "stores/TasksStore";
 import PomodoroStore from "stores/PomodoroStore";
 import TaskActions from "actions/TaskActions";
 import * as PomodoroActions from "actions/PomodoroActions";
+import formatDuration from "services/TimeFormatter";
 import { FlatButton, Paper } from "material-ui";
 import { branch } from "components/helpers";
-
-function ljust(stringish, minLength, padder = " ") {
-  let string = stringish.toString();
-  let paddingLength = minLength - string.length;
-  if (paddingLength < 0) {
-    return string;
-  }
-  return new Array( paddingLength + 1 ).join(padder) + string;
-}
-
-function formatDuration(milliseconds) {
-  let seconds = Math.round(milliseconds / 1000);
-  if (seconds === 0) {
-    return "00:00";
-  }
-
-  let result = [
-    60, // 60 seconds in a minute
-    60, // 60 minutes in an hour
-  ].map( (count) => {
-    let n = seconds % count;
-    seconds = Math.floor(seconds / count);
-    return ljust(n, 2, "0");
-  });
-
-  return result.reverse().join(":");
-}
 
 let TaskDetailScreen = React.createClass({
 
