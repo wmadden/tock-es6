d887aee60c893bf491842aec91a99d26f1e8a06c
Will Madden <w.a.madden@gmail.com>
2015-05-30 14:37:15 +0200

    Load tasks on boot

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import React from "react";
 import App from "./ui/App";
-
+import { getUnfinishedTasks } from "services/PersistenceService";
+import * as TaskActions from "actions/TaskActions";
 /*eslint-disable no-unused-vars */
 import AlarmService from "./services/AlarmService";
 /*eslint-enable no-unused-vars */
+
+getUnfinishedTasks().then((tasks) => {
+  TaskActions.tasksLoaded(tasks);
+});
 
 React.render(<App />, document.body);
