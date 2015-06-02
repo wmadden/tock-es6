1ceb0740e52bd130ac895e5fa89050aa92dda8e5
Will Madden <w.a.madden@gmail.com>
2015-06-02 09:16:14 +0200

    Hook up done button on list and order tasks

--- src/elements/TaskList.js
+++ src/elements/TaskList.js
@@ -1,42 +1,50 @@
 import React from "react";
-import { IconButton, SvgIcon, Paper } from "material-ui";
+import classnames from "classnames";
+import { IconButton, SvgIcon } from "material-ui";
 let TaskList = React.createClass({
 
   propTypes: {
-    selectTaskAndStartPomodoro: React.PropTypes.func.isRequired,
     selectTask: React.PropTypes.func.isRequired,
     deleteTask: React.PropTypes.func.isRequired,
+    markTaskFinished: React.PropTypes.func.isRequired,
     tasks: React.PropTypes.array.isRequired,
   },
 
   render() {
-    let { selectTaskAndStartPomodoro, deleteTask, selectTask } = this.props;
+    let { deleteTask, selectTask, markTaskFinished, tasks } = this.props;
+    let orderedTasks = this.orderTasks(tasks);
 
     function taskItem(task) {
-      return (
-        <li className="task cf" key={task.id} onClick={ () => selectTask(task) }>
-          { task.description }
-          { task.completedPomodoros > 0 ? ` (${task.completedPomodoros})` : "" }
-          <div className="task__actions">
+      function renderMarkFinishedButton() {
+        if (!task.finished) {
+          return (
             <IconButton
-              tooltip="Start Pomodoro"
-              onClick={ (e) => { e.stopPropagation(); selectTaskAndStartPomodoro(task); }}
+              tooltip="Mark task finished"
+              onClick={ (e) => { e.stopPropagation(); markTaskFinished(task); }}
             >
-              <SvgIcon styles={{ height: 24, width: 24 }} className="svg-ic_play_arrow_24px" />
+              <SvgIcon styles={{ height: 24, width: 24 }} className="svg-ic_done_24px" />
             </IconButton>
+          );
+        }
+        return null;
+      }
 
+      return (
+        <li
+          className={ classnames("task cf", { "task--finished": task.finished }) }
+          key={task.id} onClick={ () => selectTask(task) }
+        >
+          <span className="task__description" style={ { "float": "left" } }>
+            { task.description }
+            { task.completedPomodoros > 0 ? ` (${task.completedPomodoros})` : "" }
+          </span>
+          <div className="task__actions">
+            { renderMarkFinishedButton() }
             <IconButton
               tooltip="Delete task"
               onClick={ (e) => { e.stopPropagation(); deleteTask(task); }}
             >
               <SvgIcon styles={{ height: 24, width: 24 }} className="svg-ic_delete_24px" />
-            </IconButton>
-
-            <IconButton
-              tooltip="Mark completed"
-              onClick={ (e) => { e.stopPropagation(); }}
-            >
-              <SvgIcon styles={{ height: 24, width: 24 }} className="svg-ic_done_24px" />
             </IconButton>
           </div>
         </li>
@@ -47,11 +55,24 @@ let TaskList = React.createClass({
       <div>
         <ul className="list--no-markers">
           {
-            this.props.tasks.map( (task) => taskItem(task) )
+            orderedTasks.map( (task) => taskItem(task) )
           }
         </ul>
       </div>
     );
+  },
+
+  orderTasks(tasks) {
+    let inCompleteTasks = tasks.filter((t) => !t.finished);
+    return inCompleteTasks.sort((taskA, taskB) => {
+      if (taskA.id < taskB.id) {
+        return -1;
+      }
+      if (taskA.id > taskB.id) {
+        return 1;
+      }
+      return 0;
+    });
   },
 
 });
