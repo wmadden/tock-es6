d934f69829955ec526b7a8d065053217419cc58e
Will Madden <w.a.madden@gmail.com>
2015-06-13 12:44:28 +0200

    Initialise `NotificationService` during boot

--- src/actions/bootAction.js
+++ src/actions/bootAction.js
@@ -2,12 +2,16 @@ import React from "react";
 import App from "ui/App";
 import { getUnfinishedTasks } from "services/PersistenceService";
 import * as TaskActions from "actions/TaskActions";
+
 // We import the AlarmService here even though we don't need it so that it registers its dispatcher
 /*eslint-disable no-unused-vars */
 import AlarmService from "services/AlarmService";
 /*eslint-enable no-unused-vars */
 
+import NotificationService from "services/NotificationService";
+
 function bootAction(mountPoint) {
+  NotificationService.init();
   getUnfinishedTasks().then((tasks) => {
     TaskActions.tasksLoaded(tasks);
   });
